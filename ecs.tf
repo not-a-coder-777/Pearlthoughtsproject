33a70c9b841483b0ec7d4d88ac3ee7c169b367b8
not-a-coder-777 <101509695+not-a-coder-777@users.noreply.github.com>
2024-06-09 12:11:51 +0530

    Create ecs.tf

---
+++ ecs.tf
@@ -0,0 +1,23 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.app_name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = var.app_name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name  = var.app_name
+    image = "${aws_ecr_repository.app.repository_url}:latest"
+    portMappings = [{
+      containerPort = var.app_port
+      hostPort      = var.app_port
+    }]
+  }])
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+}
